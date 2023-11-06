7f5573d48ff596dbd6748459b90788b0374733c3
kamcho <njokevin9@gmail.com>
2023-11-06 15:28:42 +0300

    calback2

--- PSAAI/settings.py
+++ PSAAI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*1r4hwdyal=y_j%*m&+-_4!@j)33!9a(z*k_%71c($@&71fbue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','*', '16.170.243.46','http://crimsons-analytics.com/','crimsons-analytics.com']
+ALLOWED_HOSTS = ['127.0.0.1','*','http://crimsons-analytics.com/','crimsons-analytics.com']
 CSRF_TRUSTED_ORIGINS = ["http://16.170.243.46","https://107a-154-123-60-86.ngrok-free.app"]
 
 # Application definition
