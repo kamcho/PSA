cbb4ad7f0538edd007c1eec6990035360e754f31
kamcho <njokevin9@gmail.com>
2023-11-08 10:55:33 +0300

    uniform

--- PSAAI/settings.py
+++ PSAAI/settings.py
@@ -183,7 +183,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'Users.MyUser'
 LOGIN_REDIRECT_URL = 'redirect'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'uni_form'
 LOGIN_URL = 'Sign-In/'
 
 
